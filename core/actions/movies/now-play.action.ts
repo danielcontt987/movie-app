7cd1c21d8e89226dea1825f00a428d8873b02790
danielcontt987 <josedanielcontrerasperez510@gmail.com>
2026-02-26 16:13:16 -0600

    Mapear respuestas a objetos personalizados

--- core/actions/movies/now-play.action.ts
+++ core/actions/movies/now-play.action.ts
@@ -1,11 +1,15 @@
 import { movieApi } from '@/core/api/movie-api';
 import { MovieDBResponse } from '@/infrastructure/interfaces/moviedb-response';
+import { MovieMapper } from '@/infrastructure/mappers/movie.mapper';
 
 export const nowPlayAction = async () => {
     try {
         //generic request <>
         const {data} = await movieApi.get<MovieDBResponse>('/now_playing');
+        const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie);
+        console.log(movies);
         
+        return movies;
         // console.log("SUCCESS =>", response.data);
     } catch (error: any) {
         console.log("STATUS =>", error?.response?.status);
