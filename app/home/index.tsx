c2e98f300aeef4047a34ac88afb950245a438b75
danielcontt987 <josedanielcontrerasperez510@gmail.com>
2026-02-26 17:27:38 -0600

    Usando el hook de safe area

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,15 +1,25 @@
 import { useMovies } from '@/presentations/hooks/useMovies';
-import { Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
   const { nowPlayingQuery } = useMovies();
+  const safeArea = useSafeAreaInsets();
+  if (nowPlayingQuery.isLoading) {
+    return (
+      <View className='justify-center items-center flex-1'>
+        <ActivityIndicator size='large' color='purple' />
+      </View>
+    )
+  }
+
   return (
-    <View>
-      <Text>
-        {JSON.stringify(nowPlayingQuery.data)}
-      </Text>
+    <View className='mt-2' style={{ paddingTop: safeArea.top }}>
+        <Text className='text-3xl font-bold mb-2 px-4'>Home Screen</Text>
+        {/* Carrusel de películas en cartelera */}
     </View>
   )
+
 }
 
 export default HomeScreen
